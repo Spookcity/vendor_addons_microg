bddc6ac6d239c676acb827fb6b43f7289fbe6a56
Rick M <spookcity138@gmail.com>
2021-01-10 13:26:04 +0100

    Add config.mk

---
+++ config.mk
@@ -0,0 +1,10 @@
+# MicroG stuff
+PRODUCT_PACKAGES += \
+    com.google.android.maps \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    MozillaNlpBackend \
+    NominatimNlpBackend
